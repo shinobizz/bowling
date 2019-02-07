a66789aca53391fb34b1236ddca6ebf69852a4c3
Alejandra Pedroche <alejandrapedroche@mac-0614-009.oficinas.atrapalo.com>
2019-02-07 11:10:31 +0100

    spare 'works' refactor needed

--- src/Bowling.php
+++ src/Bowling.php
@@ -4,9 +4,24 @@ class Bowling
 {
     public function score($rolls){
         $score = 0;
+        $accumulated_points = 0;
         foreach ($rolls as $roll){
-            $score += $roll[0];
+            if ($accumulated_points > 0){
+                $score += $accumulated_points + $roll[0];
+                $accumulated_points = 0;
+            }
+            if($roll[0] + $roll[1] == 10){
+                $accumulated_points = 10;
+            }else{
+                $score += $roll[0] + $roll[1];
+            }
         }
+
+        if ($accumulated_points > 0){
+            $score += $accumulated_points;
+            $accumulated_points = 0;
+        }
+
         return $score;
     }
 }
--- tests/BowlingTest.php
+++ tests/BowlingTest.php
@@ -33,4 +33,27 @@ class BowlingTest extends \PHPUnit\Framework\TestCase
         $this->assertEquals(50, $this->sut->score($rolls));
     }
 
+    /**
+     * @test
+     */
+    public function getScoreAllOneAndTwoRolls()
+    {
+        $rolls = [];
+        for($i=0;$i<10;$i++){
+            array_push($rolls, [1, 2]);
+        }
+        $this->assertEquals(30, $this->sut->score($rolls));
+    }
+
+    /**
+     * @test
+     */
+    public function getScoreAllFiveAndSpareRolls()
+    {
+        $rolls = [];
+        for($i=0;$i<10;$i++){
+            array_push($rolls, [5, 5]);
+        }
+        $this->assertEquals(145, $this->sut->score($rolls));
+    }
 }
